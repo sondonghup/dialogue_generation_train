0e879fd00f5380c9854e694b84bc29bca03812b8
sondonghup <sondonghup@naver.com>
2023-02-28 21:35:24 +0900

    edit repository

---
+++ dataset.py
@@ -0,0 +1,104 @@
+import os
+import json
+import random
+import re
+import torch
+from tqdm import tqdm
+from torch.utils.data import Dataset
+
+def load_dataset(data_dir, sep_token: str, eos_token: str):
+    all_utterances = list()
+
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+    for file in os.listdir(data_dir):
+        if file.endswith('json'):
+            with open(f'{data_dir}{file}')as f:
+                file_datas = json.load(f)
+            print(f'======= {file} =======')
+            for file_data in tqdm(file_datas['data']):
+                participant_id = None
+                utterances = str()
+
+                if file_data['header']['dialogueInfo']['numberOfParticipants'] != 2:
+                    continue
+                
+                for utterance in file_data['body']:
+                    if participant_id is None:
+                        utterances += utterance['utterance']
+                    elif participant_id == utterance['participantID']:
+                        utterances += ' ' + utterance['utterance']
+                    else:
+                        utterances += sep_token + utterance['utterance']
+                    
+                    participant_id = utterance['participantID']
+                utterances += eos_token
+                all_utterances.append(utterances)
+
+    return all_utterances
+
+class DialogueDataset(Dataset):
+    def __init__(self, datas, tokenizer) -> None:
+        super(DialogueDataset, self).__init__()
+        self.tokenizer = tokenizer
+        self.input_ids = list()
+        self.attention_mask = list()
+        self.labels = list()
+
+        print(f"model_max_length : {self.tokenizer.model_max_length}")
+
+        for data in tqdm(datas):
+            encode_data = self.tokenizer(data, truncation = True, max_length = tokenizer.model_max_length)
+            self.input_ids.append(encode_data['input_ids'])
+            self.attention_mask.append(encode_data['attention_mask'])
+            self.labels.append(encode_data['input_ids'])
+
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, index):
+        return self.input_ids[index], self.attention_mask[index], self.labels[index]
+
+def collate_fn(batch, pad_token_id):
+    def seq_length_(p):
+        return len(p[0])
+    # print(f'batch : {type(batch)}')  
+    # print(f'pad_token_id : {pad_token_id}')         
+    # print(f'seq_length : {seq_length_}')
+    max_seq_sample = max(batch, key=seq_length_)[0]
+    max_seq_size = len(max_seq_sample)
+
+    batch_size = len(batch)
+
+    input_ids = torch.zeros(batch_size, max_seq_size).fill_(pad_token_id).long()
+    attention_masks = torch.zeros(batch_size, max_seq_size).fill_(pad_token_id).long()
+    labels = torch.zeros(batch_size, max_seq_size).fill_(pad_token_id).long()
+    # input_ids = torch.full((batch_size, max_seq_size), pad_token_id).long()
+    # attention_masks = torch.full((batch_size, max_seq_size), pad_token_id).long()
+    # labels = torch.full((batch_size, max_seq_size), pad_token_id).long()
+
+    for idx in range(batch_size):
+        sample = batch[idx]
+        sample_input_ids = sample[0]
+        sample_attention_masks = sample[1]
+        sample_labels = sample[2]
+
+        '''
+        y = tensor.new_tensor(x, requires_grad = True)
+        -> 파라미터가 무엇이든 간에 이를 읽어서 leaf variable로 생성한다.
+        y = x.clone.detach()
+        -> computational graph에서 더이상 필요하지 않을 때 사용할 수 있다. computational graph에서 분리 할때
+        -> y를 계산해도 x에 영향 없음 weight를 통해 특정 작업을 하고 싶을때 이용
+        y = torch.empty_like(x).copy_(x)
+        -> y에 gradient가 흐를수 있음
+        y = torch.tensor(x)
+        -> 명확하고 빠른 방법
+        '''
+
+        input_ids[idx].narrow(0, 0, len(sample_input_ids)).copy_(torch.LongTensor(sample_input_ids))
+        attention_masks[idx].narrow(0, 0, len(sample_attention_masks)).copy_(torch.LongTensor(sample_attention_masks))
+        labels[idx].narrow(0, 0, len(sample_labels)).copy_(torch.LongTensor(sample_labels))
+
+    return input_ids, attention_masks, labels
---
+++ train.py
@@ -0,0 +1,110 @@
+import argparse
+import torch
+import torch.nn as nn
+import numpy as np
+import random
+import wandb
+from torch.utils.data import DataLoader
+from transformers import GPT2LMHeadModel, AutoTokenizer
+from functools import partial
+from trainer import DGTrainer
+
+from dataset import load_dataset, DialogueDataset, collate_fn
+
+
+def main(parser):
+    data_loaders = dict()
+    args = parser.parse_args()
+
+    wandb.init(project = 'dialogue generation')
+
+    torch.manual_seed(42)
+    torch.backends.cudnn.benchmark = False # cudnn은 convolution을 수행하는 과정에서 가장 적합한 알고리즘을 선정해 수행
+    torch.backends.cudnn.determinitic = True # 
+    torch.use_deterministic_algorithms(True)
+    np.random.seed(42)
+    random.seed(42)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'device : {device}')
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model,
+                                                bos_token = '</s>',
+                                                eos_token = '</s>',
+                                                unk_token = '<unk>',
+                                                pad_token = '<pad>',
+                                                mask_token = '<mask>',
+                                                model_max_length = 1024)
+    tokenizer.add_tokens(['<sep>'])
+
+    model = GPT2LMHeadModel.from_pretrained(args.pretrained_model)
+    model.resize_token_embeddings(len(tokenizer)) # 변경된 토큰의 수만큼 임베딩을 변경
+    model = nn.DataParallel(model).to(device)
+
+    train_datasets = load_dataset(args.train_dir, '<sep>', '<eos>')
+    train_dataset = DialogueDataset(train_datasets, tokenizer)
+    valid_datasets = load_dataset(args.valid_dir, '<sep>', '<eos>')
+    valid_dataset = DialogueDataset(valid_datasets, tokenizer)
+
+    print(f"batch_size : {args.batch_size}")
+
+    data_loaders['train'] = DataLoader(dataset = train_dataset,
+                                        num_workers=args.num_workers,
+                                        collate_fn=partial(collate_fn, pad_token_id = tokenizer.pad_token_id ),
+                                        batch_size=args.batch_size,
+                                        drop_last=False)
+    
+    data_loaders['valid'] = DataLoader(dataset = valid_dataset,
+                                        num_workers=args.num_workers,
+                                        collate_fn=partial(collate_fn, pad_token_id = tokenizer.pad_token_id ),
+                                        batch_size=args.batch_size,
+                                        drop_last=False)
+
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer=optimizer,
+                                                    max_lr = args.learning_rate,
+                                                    epochs=args.num_epochs,
+                                                    steps_per_epoch=len(data_loaders['train']),
+                                                    anneal_strategy='linear'
+    )
+    
+    DGTrainer(
+        model = model,
+        train_loader = data_loaders['train'],
+        valid_loader = data_loaders['valid'],
+        optimizer = optimizer,
+        scheduler = scheduler,
+        num_epochs = args.num_epochs,
+        device = device,
+        tokenizer = tokenizer,
+        gradient_clip_val = args.gradient_clip_val,
+        log_every = args.log_every,
+        save_every = args.save_every,
+        save_dir = args.save_dir,
+        accumulate_grad_batches = args.accumulate_grad_batches
+    ).fit()
+    
+
+
+
+    return datasets
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--pretrained_model', type=str, default='skt/kogpt2-base-v2')
+    parser.add_argument('-t', '--train_dir', type = str, default='/Users/sondonghyeob/Downloads/SNS_ko/Training/SNS_ko_train/')
+    parser.add_argument('-v', '--valid_dir', type = str, default='/Users/sondonghyeob/Downloads/SNS_ko/Validation/SNS_ko_valid/')
+    parser.add_argument('-d', '--save_dir', type = str, default='checkpoints')
+    parser.add_argument('-w', '--num_workers', type = int, default=4)
+    parser.add_argument('-b', '--batch_size', type = int, default=16)
+    parser.add_argument('-l', '--learning_rate', type = int, default=3e-5)
+    parser.add_argument('-e', '--num_epochs', type = int, default=5)
+    parser.add_argument('-g', '--gradient_clip_val', type = float, default=1.0 )
+    parser.add_argument('-o', '--log_every', type=int, default=20)
+    parser.add_argument('-a', '--accumulate_grad_batches', type = int, default=1)
+    parser.add_argument('-s', '--save_every', type=int, default=10_000)
+
+    datasets = main(parser)
+    # for train_dataset in datasets['train']:
+    #     print(train_dataset)
+    #     input()    
